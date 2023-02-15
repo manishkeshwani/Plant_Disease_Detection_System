d090cf4d95e8c3935d59c05b551edf510b5015bf
Manish Keshwani <113196913+manishkeshwani@users.noreply.github.com>
2023-02-15 13:25:08 +0530

    Add files via upload
    The existing method for plant disease detection is simply naked eye observation by experts through which identification and detection of plant diseases is done. For doing so, a large team of experts as well as continuous monitoring of plant is required, which costs very high when we do with large farms. At the same time, in some countries, farmers do not have proper facilities or even idea that they can contact to experts. Due to which consulting experts even cost high as well as time consuming too. In such conditions, Leaf Doctor proves to be beneficial in monitoring large fields of crops. Automatic detection of the diseases by just seeing the symptoms on the plant leaves makes it easier as well as cheaper.

---
+++ Leaf_Doctor.py
@@ -0,0 +1,185 @@
+import streamlit as st
+import numpy as np
+from tensorflow.keras.utils import load_img
+import tensorflow as tf
+
+from streamlit_option_menu import option_menu
+
+with st.sidebar:
+    selected = option_menu(menu_title = "Leaf Doctor",
+                           
+                           options=['Apple','Tomato','Potato','Corn','Cherry','Pepper','Peach','Grapes','Strawberry','Orange','Rice','About'],
+                           
+                           icons=['app','app','app','app','app','app','app','app','app','app','app','app'],
+                           
+                           menu_icon = 'cast',
+                                                     
+                           orientation = 'vertical',
+                           
+                           default_index = 0)
+
+
+
+# funtion to predict the disease from trained model
+def predict(model,img,class_names):
+    images = load_img(img,target_size =(256,256)) 
+    img_array = tf.expand_dims(images,0)
+    predictions = model.predict(img_array)
+    predicted_class = class_names[np.argmax(predictions[0])]
+    confidence = round(100 * (np.max(predictions[0])), 2)
+    return predicted_class, confidence
+
+
+# Funtion to print image and predicted result
+def image_model_prediction(img,model,class_names):
+    st.image(img,width=700)
+    MODEL = tf.keras.models.load_model(model)
+    if st.button('Predict'):      
+      classs , comfidence = predict(MODEL,img,class_names)
+      if(classs != "Healthy"):
+        st.success(f"The leaf Disease is {classs} which is {comfidence}% confirmed")
+      else:
+        st.success(f"The leaf is {classs} {comfidence}% confirmed")
+
+    
+
+# Apple Page
+if(selected == 'Apple'):
+    st.title("Apple Disease Detection")
+    st.markdown('---')
+    apple = st.file_uploader("Please Upload an Image",type=['jpg','png','jpeg'],key="1")
+    class_names = {0:'Apple__Scab', 
+                    1:'Black_rot', 
+                    2:'Cedar_apple_rust', 
+                    3:'Healthy'}
+
+    if apple is not None:
+      image_model_prediction(apple,"apple_model",class_names)
+        
+
+# Tomato Page
+if(selected == 'Tomato'):
+    st.title("Tomato Disease Detection")
+    st.markdown('---')
+    tomato = st.file_uploader("Please Upload an Image",type=['jpg','png','jpeg'],key="2")
+    class_names = {0:'Tomato Bacterial_spot',
+                    1: 'Tomato Early_blight',
+                    2: 'Tomato Late_blight', 
+                    3:'Tomato Leaf_Mold', 
+                    4:'Tomato Septoria_leaf_spot',
+                    5: 'Tomato Spider_mites Two-spotted_spider_mite', 
+                    6:'Tomato Target_Spot',
+                    7: 'Tomato_Yellow_Leaf_Curl_Virus',
+                    8: 'Tomato_mosaic_virus',
+                    9: 'Healthy'}
+    if tomato is not None:
+      image_model_prediction(tomato,"tomato_model",class_names)
+
+
+# Potato Page
+if(selected == 'Potato'):
+    st.title("Potato Disease Detection")
+    st.markdown('---')
+    potato = st.file_uploader("Please Upload an Image",type=['jpg','png','jpeg'],key="3")
+    class_names = {0 : 'Early_Blight',
+                    1: 'Healthy',
+                    2: 'Late_Blight'}
+    if potato is not None:
+      image_model_prediction(potato,"potato_model",class_names)
+
+
+# Corn Page
+if(selected == 'Corn'):
+    st.title("Corn Disease Detection")
+    st.markdown('---')
+    corn = st.file_uploader("Please Upload an Image",type=['jpg','png','jpeg'],key="4")
+    class_names = {0:'Blight',
+                  1:'Common_Rust',
+                  2:'Gray_Leaf_Spot',
+                  3:'Healthy'}
+    if corn is not None:
+        image_model_prediction(corn,"corn_model",class_names)
+
+
+# Cherry Page
+if(selected == 'Cherry'):
+    st.title("Cherry Disease Detection")
+    st.markdown('---')
+    cherry = st.file_uploader("Please Upload an Image",type=['jpg','png','jpeg'],key="5")
+    class_names = {0:'Powdery_mildew', 1:'Healthy'}
+    if cherry is not None:
+        image_model_prediction(cherry,"cherry_model",class_names)
+
+
+# Pepper Page
+if(selected == 'Pepper'):
+    st.title("Pepper Disease Detection")
+    st.markdown('---')
+    pepper = st.file_uploader("Please Upload an Image",type=['jpg','png','jpeg'],key="6")
+    class_names = {0:'Bacterial_spot',1:'Healthy'}
+    if pepper is not None:
+        image_model_prediction(pepper,"pepper_model",class_names)
+
+
+# Peach Page
+if(selected == 'Peach'):
+    st.title("Peach Disease Detection")
+    st.markdown('---')
+    peach = st.file_uploader("Please Upload an Image",type=['jpg','png','jpeg'],key="7")
+    class_names = {0:'Bacterial_spot',1:'Healthy'}
+    if peach is not None:
+        image_model_prediction(peach,"peach_model",class_names)
+
+
+# Grapes Page
+if(selected == 'Grapes'):
+    st.title("Grapes Disease Detection")
+    st.markdown('---')
+    grapes = st.file_uploader("Please Upload an Image",type=['jpg','png','jpeg'],key="8")
+    class_names = {0:'Grape Black_rot',
+                    1: 'Grape Esca_(Black_Measles)',
+                    2: 'Grape Leaf_blight_(Isariopsis_Leaf_Spot)',
+                    3: 'Healthy'}
+
+    if grapes is not None:
+      image_model_prediction(grapes,"grapes_model",class_names)
+
+
+# Strawberry Page
+if(selected == 'Strawberry'):
+    st.title("Strawberry Disease Detection")
+    st.markdown('---')
+    strawberry = st.file_uploader("Please Upload an Image",type=['jpg','png','jpeg'],key="9")
+    class_names = {0:'Leaf_scorch',1: 'Healthy'}
+    if strawberry is not None:
+        image_model_prediction(strawberry,"strawberry_model",class_names)
+
+
+# Orange Page
+if(selected == 'Orange'):
+    st.title("Orange Disease Detection")
+    st.markdown('---')
+    orange = st.file_uploader("Please Upload an Image",type=['jpg','png','jpeg'],key="10")
+    class_names = { 0: 'Haunglongbing_(Citrus_greening)',
+                    1: 'Healthy'}
+
+    if orange is not None:
+      image_model_prediction(orange,"orange_model",class_names)
+
+
+# Rice Page
+if(selected == 'Rice'):
+    st.title("Rice Disease Detection")
+    st.markdown('---')
+    rice = st.file_uploader("Please Upload an Image",type=['jpg','png','jpeg'],key="11")
+    class_names = {0:'Bacterial leaf blight', 1:'Brown spot', 2:'Leaf smut'}
+    if rice is not None:
+      image_model_prediction(rice,"rice_model",class_names)
+
+
+# About Page
+if(selected == 'About'):
+    st.title("About")
+    st.markdown('---')
+    st.write("The existing method for plant disease detection is simply naked eye observation by experts through which identification and detection of plant diseases is done. For doing so, a large team of experts as well as continuous monitoring of plant is required, which costs very high when we do with large farms. At the same time, in some countries, farmers do not have proper facilities or even idea that they can contact to experts. Due to which consulting experts even cost high as well as time consuming too. In such conditions, Leaf Doctor proves to be beneficial in monitoring large fields of crops. Automatic detection of the diseases by just seeing the symptoms on the plant leaves makes it easier as well as cheaper.")
+    
---
+++ pepper_cnn.py
@@ -0,0 +1,129 @@
+import tensorflow as tf
+from tensorflow.keras import models, layers
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.utils import load_img
+
+IMAGE_SIZE = 256
+BATCH_SIZE = 32
+CHANNELS = 3
+EPOCHS = 50
+dataset = tf.keras.preprocessing.image_dataset_from_directory(
+    "pepper",
+    shuffle = True,
+    image_size = (IMAGE_SIZE,IMAGE_SIZE),
+    batch_size = BATCH_SIZE
+)
+
+class_names = dataset.class_names
+print(class_names)
+
+def get_dataset_partitions_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+    assert (train_split + test_split + val_split) == 1
+    
+    ds_size = len(ds)
+    
+    if shuffle:
+        ds = ds.shuffle(shuffle_size, seed=12)
+    
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
+    
+    train_ds = ds.take(train_size)    
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
+    
+    return train_ds, val_ds, test_ds
+
+
+train_ds, val_ds, test_ds = get_dataset_partitions_tf(dataset)
+len(train_ds)
+len(test_ds)
+len(val_ds)
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size = tf.data.AUTOTUNE)
+val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size = tf.data.AUTOTUNE)
+test_ds = test_ds.cache().shuffle(1000).prefetch(buffer_size = tf.data.AUTOTUNE)
+
+
+resize_and_rescale = tf.keras.Sequential([
+  layers.experimental.preprocessing.Resizing(IMAGE_SIZE, IMAGE_SIZE),
+  layers.experimental.preprocessing.Rescaling(1./255),
+])
+
+data_augmentation = tf.keras.Sequential([
+  layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
+  layers.experimental.preprocessing.RandomRotation(0.2),
+])
+
+train_ds = train_ds.map(
+    lambda x, y: (data_augmentation(x, training=True), y)
+).prefetch(buffer_size=tf.data.AUTOTUNE)
+
+print(len(train_ds))
+print(len(test_ds))
+print(len(val_ds))
+
+input_shape = (BATCH_SIZE, IMAGE_SIZE, IMAGE_SIZE, CHANNELS)
+n_classes = 2
+
+model = models.Sequential([
+    resize_and_rescale,
+    layers.Conv2D(32, kernel_size = (3,3), activation='relu', input_shape=input_shape),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64,  kernel_size = (3,3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64,  kernel_size = (3,3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(n_classes, activation='softmax'),
+])
+
+model.build(input_shape=input_shape)
+
+
+model.summary()
+
+model.compile(
+    optimizer = 'adam',
+    loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = False),
+    metrics = ['accuracy']
+)
+
+history = model.fit(
+    train_ds,
+    epochs = EPOCHS,
+    batch_size = BATCH_SIZE,
+    verbose = 1,
+    validation_data = val_ds
+)
+
+model_version = 'pepper_model'
+model.save(f"../model/{model_version}")
+model.save('pepper_model.h5')
+
+scores = model.evaluate(test_ds)
+
+
+def predict(model,img):
+    images = load_img(img) 
+    img_array = tf.expand_dims(images,0)
+    predictions = model.predict(img_array)
+    predicted_class = class_names[np.argmax(predictions[0])]
+    confidence = round(100 * (np.max(predictions[0])), 2)
+    return predicted_class, confidence
+
+classs , comfidence = predict(model,"D:/Leaf_disease_prediction_system/pepper/Bacterial_spot/0a4c007d-41ab-4659-99cb-8a4ae4d07a55___NREC_B.Spot 1954.JPG")
+print(classs,comfidence)
+MODEL = tf.keras.models.load_model("../model/pepper_model")
+classs , comfidence = predict(MODEL,"D:/Leaf_disease_prediction_system/pepper/healthy/0a3f2927-4410-46a3-bfda-5f4769a5aaf8___JR_HL 8275.JPG")
+print(classs,comfidence)
+
+# class_names = {0:'Bacterial_spot',1:'Healthy'}
